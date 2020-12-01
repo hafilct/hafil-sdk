67411c6a2bba761f3c78e3006de3bda3bb33759b
Hafil c <hafilct@gmail.com>
2020-12-01 18:44:12 +0530

    check -attachment updated

--- examples/add-check-attachment.php
+++ examples/add-check-attachment.php
@@ -7,20 +7,22 @@ use OnlineCheckWriter\OnlineCheckWriter;
     $environment = "LOCAL" ; // SANDBOX , LIVE
     $ocw         = new OnlineCheckWriter($token,$environment);
 
-    //Example 1-Add local file
+    $checkId ="6VoW41y71K4589l"; 
+
+    //Example 1-Add attachment from local file to exisiting check
     $attachment=[
         "attachment"=>"@PATH\Test.pdf",
     ];
 
-    $response=$ocw->checkAttachments()->create($attachment,$checkid);
+    $response=$ocw->checkAttachments()->create($attachment,$checkId);
     print_r($response);
 
 
-    //Example 2 -Add remote url
+    //Example 2 -Add attachment from remote url to exisiting check
      $attachment=[
         "fileUrl"=>"@PATH\Test.pdf",
     ];
 
-    $response=$ocw->checkAttachments()->create($attachment,$checkid);
+    $response=$ocw->checkAttachments()->create($attachment,$checkId);
     print_r($response);
     
